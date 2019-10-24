b94cc27a71ac043fcafc6e4c08149e4ad8ce3f07
elijah johnston <eli@millinno.com>
2019-10-24 11:10:58 +0800

    livecode306init

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar'
+import { loadDynamicBannerText } from '../components/banner'
+
+console.log('Hello World from Webpacker')
+initUpdateNavbarOnScroll();
+loadDynamicBannerText();
